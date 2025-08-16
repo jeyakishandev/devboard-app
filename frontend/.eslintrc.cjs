e05e0ff3b1e14a307a81587488229dc2e01251cb
jeyakishandev <k.jeyakishan@gmail.com>
2025-08-16 13:17:02 +0200

    feat(frontend): init React + TypeScript with Vite

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  env: { browser: true, es2022: true },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  rules: {}
+};
